b3469813fb7219beae687682e5cf96fac95f0b8c
Dhruv Mehta <flashxx001@gmail.com>
2024-07-12 20:16:30 +0530

    Created Skeleton

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="flex flex-col">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <h1 className="text-center font-semibold text-3xl sm:text-5xl bg-yellow-600 p-4">
+      🌴 Far Away 💼
+    </h1>
+  );
+}
+function Form() {
+  return (
+    <div>
+      <h3 className="text-center font-normal text-lg sm:text-2xl bg-orange-600 p-6">
+        What stuffs do you need for Trip ?
+      </h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="flex justify-center font-semibold text-xl bg-yellow-900 min-h-[505px] text-white p-4">
+      LIST
+    </div>
+  );
+}
+function Stats() {
+  return (
+    <footer className="w-full text-center">
+      <em className="pt-4">
+        You have X items on your list, and you already packed X (X%)
+      </em>
+    </footer>
+  );
+}
