440d72034afc312c9147767f5e7ccfa07503c406
Dhruv <dhruvmehta1311@gmail.com>
2024-07-18 21:56:13 +0530

    Added useState for Description.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -24,6 +26,7 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('')
   function handleSubmit(e){
 e.preventDefault()
   }
