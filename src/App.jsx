d580744753a3c528fc900b8adde733221ff6c17c
Dhruv Mehta <dhruvmehta1311@gmail.com>
2024-07-21 16:55:03 +0530

    Made the footer dynamic also did some conditional rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
         handleDeleteItem={handleDeleteItem}
         handleUpdateItems={handleUpdateItems}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -123,11 +123,25 @@ function PackingList({ items, handleDeleteItem, handleUpdateItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="flex items-center justify-center w-full text-center bg-sky-600 h-10">
+        <em className="text-white">Start Adding your items.</em>
+      </footer>
+    );
+  }
+
+  const totalItems = items.length;
+  const itemsPacked = items.filter((item) => item.packed).length;
+  const itemsPercentage = Math.round((itemsPacked / totalItems) * 100);
   return (
     <footer className="flex items-center justify-center w-full text-center bg-sky-600 h-10">
       <em className="text-white">
-        You have X items on your list, and you already packed X (X%)
+        {itemsPercentage === 100
+          ? "You are ready to go!!"
+          : `You have ${totalItems} items on your list, and you already packed
+        ${itemsPacked} items (${itemsPercentage}%)`}
       </em>
     </footer>
   );
